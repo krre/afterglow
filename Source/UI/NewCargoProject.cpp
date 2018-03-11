cf8e4ad042a51dd8d34c1355fdeaf7f69071cc66
Vladimir Zarypov <krre31@gmail.com>
2018-03-11 15:04:44 +0200

    Setup entering new project name and directory

--- Source/UI/NewCargoProject.cpp
+++ Source/UI/NewCargoProject.cpp
@@ -1,13 +1,40 @@
 #include "NewCargoProject.h"
 #include "ui_NewCargoProject.h"
+#include "Core/Global.h"
+#include <QtCore>
+#include <QtWidgets>
 
 NewCargoProject::NewCargoProject(QWidget* parent) :
         QDialog(parent),
         ui(new Ui::NewCargoProject) {
     ui->setupUi(this);
     setFixedHeight(height());
+
+    QSettings settings(Global::getPortableSettingsPath(), QSettings::IniFormat);
+    ui->lineEditDirectory->setText(settings.value("Path/Workspace", Global::getDefaultWorkspacePath()).toString());
+
+    connect(ui->lineEditName, &QLineEdit::textChanged, this, &NewCargoProject::adjustAcceptedButton);
+    connect(ui->lineEditDirectory, &QLineEdit::textChanged, this, &NewCargoProject::adjustAcceptedButton);
+
+    adjustAcceptedButton();
 }
 
 NewCargoProject::~NewCargoProject() {
     delete ui;
+}
+
+void NewCargoProject::on_pushButtonDirectory_clicked() {
+    QString dirPath = QFileDialog::getExistingDirectory(this);
+    if (!dirPath.isEmpty()) {
+        ui->lineEditDirectory->setText(dirPath);
+    }
+}
+
+void NewCargoProject::on_buttonBox_accepted() {
+    QString projectPath = ui->lineEditDirectory->text() + "/" + ui->lineEditName->text();
+    qDebug() << projectPath;
+}
+
+void NewCargoProject::adjustAcceptedButton() {
+    ui->buttonBox->buttons().at(0)->setEnabled(!ui->lineEditName->text().isEmpty() && !ui->lineEditDirectory->text().isEmpty());
 }
--- Source/UI/NewCargoProject.h
+++ Source/UI/NewCargoProject.h
@@ -12,6 +12,11 @@ public:
     explicit NewCargoProject(QWidget* parent = 0);
     ~NewCargoProject();
 
+private slots:
+    void on_pushButtonDirectory_clicked();
+    void on_buttonBox_accepted();
+    void adjustAcceptedButton();
+
 private:
     Ui::NewCargoProject* ui;
 };
