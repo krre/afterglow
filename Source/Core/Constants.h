af90ac600e13631485fb3455608a025c7272a911
Vladimir Zarypov <krre31@gmail.com>
2018-03-13 10:09:30 +0200

    Rename WORKSPACE_DIRECTORY const to RustProjects

--- Source/Core/Constants.h
+++ Source/Core/Constants.h
@@ -6,7 +6,7 @@ const QString APP_VERSION = "0.1.0";
 const QString APP_URL = "https://github.com/krre/afterglow";
 const QString APP_SETTINGS_NAME = "afterglow.ini";
 const QString APP_COPYRIGHT = "Copyright © 2018, Vladimir Zarypov";
-const QString WORKSPACE_DIRECTORY = "CargoProjects";
+const QString WORKSPACE_DIRECTORY = "RustProjects";
 
 const int WINDOW_WIDTH = 1200;
 const int WINDOW_HEIGHT = 800;
