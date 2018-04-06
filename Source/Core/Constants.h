34718e0901b8edd1c86e67b2fcb89cb9c3e5d646
Vladimir Zarypov <krre31@gmail.com>
2018-04-06 07:11:53 +0300

    Change app version on 0.2.0

--- Source/Core/Constants.h
+++ Source/Core/Constants.h
@@ -4,7 +4,7 @@
 namespace Constants {
 
 const char APP_NAME[] = "Afterglow";
-const char APP_VERSION[] = "0.1.0";
+const char APP_VERSION[] = "0.2.0";
 const char APP_STATUS[] = "Preview Alpha";
 const char APP_URL[] = "https://github.com/krre/afterglow";
 const char APP_SETTINGS_NAME[] = "afterglow.ini";
