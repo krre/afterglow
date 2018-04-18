e2d66209a1d416bdcbf898366e1fc684e32dd7a0
Vladimir Zarypov <krre31@gmail.com>
2018-04-18 09:12:54 +0300

    Move number area digits count constant to settings

--- Source/TextEditor/TextEditor.cpp
+++ Source/TextEditor/TextEditor.cpp
@@ -113,9 +113,7 @@ int TextEditor::getLineNumberAreaWidth() {
         ++digits;
     }
 
-    const int DEFAULT_DIGITS = 4;
-    digits = qMax(DEFAULT_DIGITS, digits);
-
+    digits = qMax(Settings::getValue("editor.numberAreaDigits").toInt(), digits);
     int space = 3 + fontMetrics().width(QLatin1Char('9')) * digits;
 
     return space;
