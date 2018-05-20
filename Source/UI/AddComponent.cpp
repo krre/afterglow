0c58121f18cd8c00a3137832d11ec921f36cf52f
Vladimir Zarypov <krre31@gmail.com>
2018-05-20 14:06:11 +0300

    Add loading components into Add Component dialog

--- Source/UI/AddComponent.cpp
+++ Source/UI/AddComponent.cpp
@@ -1,10 +1,24 @@
 #include "AddComponent.h"
 #include "ui_AddComponent.h"
+#include "Core/Utils.h"
+#include "StringListModel.h"
 
 AddComponent::AddComponent(QWidget *parent) :
         QDialog(parent),
         ui(new Ui::AddComponent) {
     ui->setupUi(this);
+
+    QStringList componentList = Utils::listFromConsole("rustup component list");
+    for (int i = componentList.count() - 1; i >= 0; i--) {
+        if (componentList.at(i).contains("(default)") || componentList.at(i).contains("(installed)")) {
+            componentList.removeAt(i);
+        }
+    }
+
+    QAbstractItemModel* model = new StringListModel(componentList, this);
+    ui->listView->setModel(model);
+
+    ui->listView->setSelectionMode(QAbstractItemView::ExtendedSelection);
 }
 
 AddComponent::~AddComponent() {
