6c046c7cc43cfcbb51631e1b694fe55d1ade4716
Vladimir Zarypov <krre31@gmail.com>
2018-06-03 12:50:48 +0300

    Add IssueItem class

--- Source/UI/IssueModel.cpp
+++ Source/UI/IssueModel.cpp
@@ -1,5 +1,47 @@
 #include "IssueModel.h"
 
+IssueItem::IssueItem(const QList<Issue>& data, IssueItem* parentItem) {
+    this->parentItem = parentItem;
+    itemData = data;
+}
+
+IssueItem::~IssueItem() {
+    qDeleteAll(childItems);
+}
+
+void IssueItem::appendChild(IssueItem* child) {
+    childItems.append(child);
+}
+
+IssueItem* IssueItem::getChild(int row) {
+     return childItems.value(row);
+ }
+
+ int IssueItem::getChildCount() const {
+     return childItems.count();
+ }
+
+ int IssueItem::getColumnCount() const {
+     return itemData.count();
+ }
+
+ Issue IssueItem::getData(int column) const {
+     return itemData.value(column);
+ }
+
+ IssueItem* IssueItem::getParentItem() {
+     return parentItem;
+ }
+
+ int IssueItem::getRow() const {
+     if (parentItem) {
+         return parentItem->childItems.indexOf(const_cast<IssueItem*>(this));
+     }
+
+     return 0;
+ }
+
+
 IssueModel::IssueModel(QObject* parent) : QAbstractItemModel(parent) {
 
 }
--- Source/UI/IssueModel.h
+++ Source/UI/IssueModel.h
@@ -1,5 +1,40 @@
 #include <QAbstractItemModel>
 
+struct Issue {
+    enum class Level {
+        Note,
+        Info,
+        Warning,
+        Error
+    };
+
+    Level level;
+    QString message;
+    QString description;
+    QString filename;
+    int line;
+    int column;
+};
+
+class IssueItem {
+public:
+    explicit IssueItem(const QList<Issue>& data, IssueItem* parentItem = nullptr);
+    ~IssueItem();
+
+    void appendChild(IssueItem* child);
+    IssueItem* getChild(int row);
+    int getChildCount() const;
+    int getColumnCount() const;
+    Issue getData(int column) const;
+    int getRow() const;
+    IssueItem* getParentItem();
+
+private:
+    QList<IssueItem*> childItems;
+    QList<Issue> itemData;
+    IssueItem* parentItem;
+};
+
 class IssueModel : public QAbstractItemModel {
     Q_OBJECT
 public:
