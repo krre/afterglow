50fbe880bd95282f2948e28e44dae2dd109daadd
Vladimir Zarypov <krre31@gmail.com>
2018-06-03 08:58:52 +0300

    Add IssueDelegate class

--- Source/UI/IssueListView.cpp
+++ Source/UI/IssueListView.cpp
@@ -1,5 +1,10 @@
 #include "IssueListView.h"
+#include "IssueModel.h"
 
 IssueListView::IssueListView(QWidget* parent) : QListView(parent) {
+    setItemDelegate(new IssueDelegate(this));
+}
+
+IssueDelegate::IssueDelegate(QObject* parent) : QStyledItemDelegate(parent) {
 
 }
--- Source/UI/IssueListView.h
+++ Source/UI/IssueListView.h
@@ -1,9 +1,16 @@
 #include <QListView>
+#include <QStyledItemDelegate>
 
 class IssueListView : public QListView {
     Q_OBJECT
-
 public:
     explicit IssueListView(QWidget* parent = nullptr);
+
+};
+
+class IssueDelegate : public QStyledItemDelegate {
+    Q_OBJECT
+public:
+    IssueDelegate(QObject* parent = nullptr);
 
 };
