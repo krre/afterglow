13d2df5978a685d56f9238cb52ad480df0ac5282
Vladimir Zarypov <krre31@gmail.com>
2018-06-03 13:09:41 +0300

    Add override methods to IssueModel class

--- Source/UI/IssueModel.cpp
+++ Source/UI/IssueModel.cpp
@@ -1,6 +1,6 @@
 #include "IssueModel.h"
 
-IssueItem::IssueItem(const QList<Issue>& data, IssueItem* parentItem) {
+IssueItem::IssueItem(const QList<QVariant>& data, IssueItem* parentItem) {
     this->parentItem = parentItem;
     itemData = data;
 }
@@ -25,7 +25,7 @@ IssueItem* IssueItem::getChild(int row) {
      return itemData.count();
  }
 
- Issue IssueItem::getData(int column) const {
+ QVariant IssueItem::getData(int column) const {
      return itemData.value(column);
  }
 
@@ -43,5 +43,85 @@ IssueItem* IssueItem::getChild(int row) {
 
 
 IssueModel::IssueModel(QObject* parent) : QAbstractItemModel(parent) {
+}
 
+IssueModel::~IssueModel() {
+    delete rootItem;
+}
+
+int IssueModel::columnCount(const QModelIndex& parent) const {
+    if (parent.isValid()) {
+        return static_cast<IssueItem*>(parent.internalPointer())->getColumnCount();
+    } else {
+        return rootItem->getColumnCount();
+    }
+}
+
+QVariant IssueModel::data(const QModelIndex& index, int role) const {
+    if (!index.isValid()) return QVariant();
+
+    if (role != Qt::DisplayRole) {
+        return QVariant();
+    }
+
+    IssueItem* item = static_cast<IssueItem*>(index.internalPointer());
+
+    return item->getData(index.column());
+}
+
+Qt::ItemFlags IssueModel::flags(const QModelIndex& index) const {
+    if (!index.isValid()) return 0;
+
+    return QAbstractItemModel::flags(index);
+}
+
+QVariant IssueModel::headerData(int section, Qt::Orientation orientation, int role) const {
+    if (orientation == Qt::Horizontal && role == Qt::DisplayRole) {
+        return rootItem->getData(section);
+    }
+
+    return QVariant();
+}
+
+QModelIndex IssueModel::index(int row, int column, const QModelIndex& parent) const {
+    if (!hasIndex(row, column, parent)) return QModelIndex();
+
+    IssueItem* parentItem;
+
+    if (!parent.isValid()) {
+        parentItem = rootItem;
+    } else {
+        parentItem = static_cast<IssueItem*>(parent.internalPointer());
+    }
+
+    IssueItem* childItem = parentItem->getChild(row);
+    if (childItem) {
+        return createIndex(row, column, childItem);
+    } else {
+        return QModelIndex();
+    }
+}
+
+QModelIndex IssueModel::parent(const QModelIndex& index) const {
+    if (!index.isValid()) return QModelIndex();
+
+    IssueItem* childItem = static_cast<IssueItem*>(index.internalPointer());
+    IssueItem* parentItem = childItem->getParentItem();
+
+    if (parentItem == rootItem) return QModelIndex();
+
+    return createIndex(parentItem->getRow(), 0, parentItem);
+}
+
+int IssueModel::rowCount(const QModelIndex &parent) const {
+    IssueItem* parentItem;
+    if (parent.column() > 0) return 0;
+
+    if (!parent.isValid()) {
+        parentItem = rootItem;
+    } else {
+        parentItem = static_cast<IssueItem*>(parent.internalPointer());
+    }
+
+    return parentItem->getChildCount();
 }
--- Source/UI/IssueModel.h
+++ Source/UI/IssueModel.h
@@ -18,20 +18,20 @@ struct Issue {
 
 class IssueItem {
 public:
-    explicit IssueItem(const QList<Issue>& data, IssueItem* parentItem = nullptr);
+    explicit IssueItem(const QList<QVariant>& data, IssueItem* parentItem = nullptr);
     ~IssueItem();
 
     void appendChild(IssueItem* child);
     IssueItem* getChild(int row);
     int getChildCount() const;
     int getColumnCount() const;
-    Issue getData(int column) const;
+    QVariant getData(int column) const;
     int getRow() const;
     IssueItem* getParentItem();
 
 private:
     QList<IssueItem*> childItems;
-    QList<Issue> itemData;
+    QList<QVariant> itemData;
     IssueItem* parentItem;
 };
 
@@ -39,6 +39,20 @@ class IssueModel : public QAbstractItemModel {
     Q_OBJECT
 public:
     explicit IssueModel(QObject* parent = nullptr);
+    ~IssueModel();
+
+    QVariant data(const QModelIndex& index, int role) const override;
+    Qt::ItemFlags flags(const QModelIndex& index) const override;
+    QVariant headerData(int section, Qt::Orientation orientation,
+                        int role = Qt::DisplayRole) const override;
+    QModelIndex index(int row, int column,
+                      const QModelIndex& parent = QModelIndex()) const override;
+    QModelIndex parent(const QModelIndex& index) const override;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+
+private:
+    IssueItem *rootItem;
 
 signals:
 
