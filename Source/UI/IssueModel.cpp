97d5d06b56b05b93877c05646370c212eaebec8e
Vladimir Zarypov <krre31@gmail.com>
2018-06-06 13:09:35 +0300

    Cleanup IssueModel class

--- Source/UI/IssueModel.cpp
+++ Source/UI/IssueModel.cpp
@@ -8,8 +8,9 @@ IssueModel::~IssueModel() {
 }
 
 void IssueModel::appendMessage(const QJsonObject& message) {
-//    qDebug() << message;
     QJsonObject messageObj = message["message"].toObject();
+    QJsonObject spanObj = messageObj["spans"].toArray().at(0).toObject();
+
     Issue issue;
 
     QString level = messageObj["level"].toString();
@@ -25,24 +26,21 @@ void IssueModel::appendMessage(const QJsonObject& message) {
 
     issue.message = messageObj["message"].toString();
     issue.rendered = messageObj["rendered"].toString();
-
-    QJsonObject spanObj = messageObj["spans"].toArray().at(0).toObject();
     issue.filename = spanObj["file_name"].toString();
     issue.line = spanObj["line_start"].toInt();
     issue.column = spanObj["column_start"].toInt();
 
-    int count = issues.count();
-    insertRows(count, 1, QModelIndex());
-    QModelIndex idx = index(count, 0, QModelIndex());
-    setData(idx, static_cast<int>(issue.level), Qt::EditRole);
-    idx = index(count, 1, QModelIndex());
-    setData(idx, issue.message, Qt::EditRole);
+    beginInsertRows(QModelIndex(), issues.count(), issues.count());
+    issues.append(issue);
+    endInsertRows();
 }
 
 void IssueModel::clear() {
-    if (!issues.count()) return;
+    if (issues.isEmpty()) return;
 
-    removeRows(0, issues.count(), QModelIndex());
+    beginRemoveRows(QModelIndex(), 0, issues.count() - 1);
+    issues.clear();
+    endRemoveRows();
 }
 
 int IssueModel::columnCount(const QModelIndex& parent) const {
@@ -50,36 +48,7 @@ int IssueModel::columnCount(const QModelIndex& parent) const {
     return 1;
 }
 
-bool IssueModel::insertRows(int row, int count, const QModelIndex& parent) {
-    Q_UNUSED(parent)
-
-    beginInsertRows(QModelIndex(), issues.count(), issues.count());
-    qDebug() << "IssueModel::insertRows" << row << count;
-    for (int i = 0; i < count; i++) {
-        Issue issue;
-        issues.insert(row, issue);
-    }
-
-    endInsertRows();
-
-    return true;
-}
-
-bool IssueModel::removeRows(int row, int count, const QModelIndex& parent) {
-    Q_UNUSED(parent);
-
-    beginRemoveRows(QModelIndex(), row, row + count - 1);
-
-    for (int i = 0; i < count; ++i)
-        issues.removeAt(i);
-
-    endRemoveRows();
-
-    return true;
-}
-
 QVariant IssueModel::data(const QModelIndex& index, int role) const {
-    qDebug() << "IssueModel::data" << index << role << issues.count();
     if (!index.isValid()) return QVariant();
 
     if (index.row() >= issues.count() || index.row() < 0) {
@@ -107,45 +76,8 @@ QVariant IssueModel::data(const QModelIndex& index, int role) const {
     return QVariant();
 }
 
-bool IssueModel::setData(const QModelIndex& index, const QVariant& value, int role) {
-    if (index.isValid() && role == Qt::EditRole) {
-        int row = index.row();
-
-        Issue issue = issues.value(row);
-
-        if (index.column() == 0) {
-            issue.level = static_cast<Issue::Level>(value.toInt());
-        } else if (index.column() == 1) {
-           issue.message = value.toString();
-        } else if (index.column() == 2) {
-            issue.rendered = value.toString();
-        } else if (index.column() == 3) {
-            issue.filename = value.toString();
-        } else if (index.column() == 4) {
-            issue.line = value.toInt();
-        } else if (index.column() == 5) {
-            issue.column = value.toInt();
-        } else {
-            return false;
-        }
-
-        issues.replace(row, issue);
-        emit(dataChanged(index, index));
-
-        return true;
-    }
-
-    return false;
-}
-
-Qt::ItemFlags IssueModel::flags(const QModelIndex& index) const {
-    if (!index.isValid()) return Qt::ItemIsEnabled;
-
-    return QAbstractItemModel::flags(index) | Qt::ItemIsEditable;
-}
-
 QModelIndex IssueModel::index(int row, int column, const QModelIndex& parent) const {
-    if (parent.isValid()) return QModelIndex();
+    Q_UNUSED(parent)
     return createIndex(row, column);
 }
 
@@ -156,6 +88,5 @@ QModelIndex IssueModel::parent(const QModelIndex& index) const {
 
 int IssueModel::rowCount(const QModelIndex &parent) const {
     Q_UNUSED(parent)
-    qDebug() << "issueModel::rowCount" << parent << issues.count();
     return issues.count();
 }
--- Source/UI/IssueModel.h
+++ Source/UI/IssueModel.h
@@ -26,15 +26,10 @@ public:
     void clear();
 
     QVariant data(const QModelIndex& index, int role) const override;
-    bool setData(const QModelIndex& index, const QVariant& value, int role) override;
-    Qt::ItemFlags flags(const QModelIndex& index) const override;
     QModelIndex index(int row, int column, const QModelIndex& parent = QModelIndex()) const override;
     QModelIndex parent(const QModelIndex& index) const override;
     int rowCount(const QModelIndex& parent = QModelIndex()) const override;
     int columnCount(const QModelIndex& parent = QModelIndex()) const override;
-
-    bool insertRows(int row, int count, const QModelIndex& parent) override;
-    bool removeRows(int row, int count, const QModelIndex& parent) override;
 
 private:
     QList<Issue> issues;
