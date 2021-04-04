e4147cd97139786238fd26b21409dc79799b7e3b
Vladimir Zarypov <krre31@gmail.com>
2021-04-04 17:10:38 +0300

    Set application name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use antiq::window::*;
 
 fn main() {
-    let application_window = ApplicationWindow::new();
+    let mut application_window = ApplicationWindow::new();
+    application_window.set_title("Afterglow".to_string());
     println!("{}", application_window.title());
 }
