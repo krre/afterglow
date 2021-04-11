08d2d2f13f6e465d87ec75ec831e4ff3d0c55064
Vladimir Zarypov <krre31@gmail.com>
2021-04-11 16:29:11 +0300

    Fix application window title

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use antiq::window::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut application_window = ApplicationWindow::new();
-    application_window.set_title("Afterglow".to_string());
+    application_window.set_title("Afterglow");
     application::run()
 }
