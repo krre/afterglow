568e111f67b9c672b47a2f2b447f8a71bfecab7f
Vladimir Zarypov <krre31@gmail.com>
2021-04-24 18:29:58 +0300

    Use widgets from `antiq::widget` module

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use antiq::core::application;
-use antiq::window::*;
+use antiq::core::node::Node;
+use antiq::widget;
+use antiq::window::Windowed;
+use std::rc::Rc;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut application_window = ApplicationWindow::new();
-    application_window.set_title("Afterglow");
-    application::run()
+    let mut application_window = widget::ApplicationWindow::new();
+    application_window.get_mut().set_title("Afterglow");
+
+    let mut app = widget::Application::new();
+    app.add_child(Rc::new(application_window));
+    app.get().run()
 }
