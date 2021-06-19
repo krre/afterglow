73e42cde6c83084160b7bd5d1ecaa0083f362517
Vladimir Zarypov <krre31@gmail.com>
2021-06-19 17:48:03 +0300

    Set title for application window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use antiq::entity::Application;
+use antiq::widget::ApplicationWindow;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut app_window = ApplicationWindow::new();
+    app_window.set_title("Afterglow");
+
     let app = Application::new();
     app.run()
 }
