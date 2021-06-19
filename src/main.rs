dbeea99714aa34447c0ea3e13f765ff9523bcb49
Vladimir Zarypov <krre31@gmail.com>
2021-06-19 15:54:39 +0300

    Cleanup deprecated code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use antiq::core::node::Node;
-use antiq::widget;
-use antiq::window::Windowed;
-use std::rc::Rc;
+use antiq::entity::Application;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut application_window = widget::ApplicationWindow::new();
-    application_window.get_mut().set_title("Afterglow");
-
-    let mut app = widget::Application::new();
-    app.add_child(Rc::new(application_window));
-    app.get().run()
+    let app = Application::new();
+    app.run()
 }
