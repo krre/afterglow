a2b1388870aa317a1d9d748bfeb6a84200a2151e
Vladimir Zarypov <krre31@gmail.com>
2021-06-30 18:45:08 +0300

    Use Box for window container

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use antiq::entity::Application;
 use antiq::widget::ApplicationWindow;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut app_window = ApplicationWindow::new();
+    let mut app_window = Box::new(ApplicationWindow::new());
     app_window.set_title("Afterglow");
 
-    let app = Application::new();
+    let mut app = Application::new();
+    app.add_window(app_window);
+
     app.run()
 }
