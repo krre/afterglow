c55d3a4ea7b5c365718eeb94551786fff4e6cce0
Vladimir Zarypov <krre31@gmail.com>
2022-11-02 07:54:15 +0200

    Reformat Const namespace

--- src/core/Const.h
+++ src/core/Const.h
@@ -2,78 +2,80 @@
 #include <QChar>
 
 namespace Const {
-    namespace Environment {
-        constexpr auto RustupHome = "RUSTUP_HOME";
-        constexpr auto CargoHome = "CARGO_HOME";
-        constexpr auto Path = "PATH";
-    }
 
-    namespace App {
-        constexpr auto Organization = "Afterglow";
-        constexpr auto Name = "Afterglow";
-        constexpr auto Version = "0.2.0";
-        constexpr auto Status = "Preview Alpha";
-        constexpr auto Url = "https://github.com/krre/afterglow";
-        constexpr auto PrefsName = "prefs.json";
-        constexpr auto Copyright = "Copyright © 2018-2022, Vladimir Zarypov";
-    }
+namespace Environment {
+    constexpr auto RustupHome = "RUSTUP_HOME";
+    constexpr auto CargoHome = "CARGO_HOME";
+    constexpr auto Path = "PATH";
+}
 
-    namespace Window {
-        constexpr auto WorkspaceDir = "RustProjects";
-        constexpr int MaxRecentFiles = 10;
-        constexpr int SeparatorAndMenuClearCount = 2;
-    }
+namespace App {
+    constexpr auto Organization = "Afterglow";
+    constexpr auto Name = "Afterglow";
+    constexpr auto Version = "0.2.0";
+    constexpr auto Status = "Preview Alpha";
+    constexpr auto Url = "https://github.com/krre/afterglow";
+    constexpr auto PrefsName = "prefs.json";
+    constexpr auto Copyright = "Copyright © 2018-2022, Vladimir Zarypov";
+}
 
-    namespace Project {
-        constexpr auto DataDir = ".afterglow";
-        constexpr auto SessionFile = "session.json";
-        constexpr auto PropertyFile = "properties.json";
-    }
+namespace Window {
+    constexpr auto WorkspaceDir = "RustProjects";
+    constexpr int MaxRecentFiles = 10;
+    constexpr int SeparatorAndMenuClearCount = 2;
+}
 
-    namespace FontAwesome {
-        constexpr auto Cog = QChar(0xf013);
-        constexpr auto Stop = QChar(0xf04d);
-        constexpr auto TrashAlt = QChar(0xf2ed);
-        constexpr auto Play = QChar(0xf04b);
-        constexpr auto ExclamationTriangle = QChar(0xf071);
-        constexpr auto TimesCircle = QChar(0xf057);
-    }
+namespace Project {
+    constexpr auto DataDir = ".afterglow";
+    constexpr auto SessionFile = "session.json";
+    constexpr auto PropertyFile = "properties.json";
+}
 
-    namespace Color {
-        constexpr auto Message = "#0000ff";
-        constexpr auto Line = "#e0eff5";
-        constexpr auto LineNumberArea = "#f0f0f0";
-        constexpr auto LineNumber = "#aaaaaa";
-        constexpr auto IssueSeparator = "#969696";
-        constexpr auto WarningIssueIcon = "#ffd800";
-        constexpr auto ErrorIssueIcon = "#ff3600";
+namespace FontAwesome {
+    constexpr auto Cog = QChar(0xf013);
+    constexpr auto Stop = QChar(0xf04d);
+    constexpr auto TrashAlt = QChar(0xf2ed);
+    constexpr auto Play = QChar(0xf04b);
+    constexpr auto ExclamationTriangle = QChar(0xf071);
+    constexpr auto TimesCircle = QChar(0xf057);
+}
 
-    }
+namespace Color {
+    constexpr auto Message = "#0000ff";
+    constexpr auto Line = "#e0eff5";
+    constexpr auto LineNumberArea = "#f0f0f0";
+    constexpr auto LineNumber = "#aaaaaa";
+    constexpr auto IssueSeparator = "#969696";
+    constexpr auto WarningIssueIcon = "#ffd800";
+    constexpr auto ErrorIssueIcon = "#ff3600";
 
-    namespace Action {
-        constexpr auto NewProject = "NewProject";
-        constexpr auto NewRustFile = "NewRustFile";
-        constexpr auto NewFile = "NewFile";
-        constexpr auto NewDirectory = "NewDirectory";
+}
 
-        constexpr auto Open = "Open";
-        constexpr auto CloseProject = "CloseProject";
+namespace Action {
+    constexpr auto NewProject = "NewProject";
+    constexpr auto NewRustFile = "NewRustFile";
+    constexpr auto NewFile = "NewFile";
+    constexpr auto NewDirectory = "NewDirectory";
 
-        constexpr auto Save = "Save";
-        constexpr auto SaveAs = "SaveAs";
-        constexpr auto SaveAll = "SaveAll";
+    constexpr auto Open = "Open";
+    constexpr auto CloseProject = "CloseProject";
 
-        constexpr auto Close = "Close";
-        constexpr auto CloseAll = "CloseAll";
-        constexpr auto CloseOther = "CloseOther";
+    constexpr auto Save = "Save";
+    constexpr auto SaveAs = "SaveAs";
+    constexpr auto SaveAll = "SaveAll";
 
-        constexpr auto Build = "Build";
-        constexpr auto Run = "Run";
-        constexpr auto Check = "Check";
-        constexpr auto Stop = "Stop";
-        constexpr auto Clean = "Clean";
+    constexpr auto Close = "Close";
+    constexpr auto CloseAll = "CloseAll";
+    constexpr auto CloseOther = "CloseOther";
 
-        constexpr auto ShowSidebar = "ShowSidebar";
-        constexpr auto ShowOutput = "ShowOutput";
-    }
+    constexpr auto Build = "Build";
+    constexpr auto Run = "Run";
+    constexpr auto Check = "Check";
+    constexpr auto Stop = "Stop";
+    constexpr auto Clean = "Clean";
+
+    constexpr auto ShowSidebar = "ShowSidebar";
+    constexpr auto ShowOutput = "ShowOutput";
+}
+
 }
