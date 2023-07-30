28c93b5eca0238ad31fe186c3a8e2894abfd42a6
Vladimir Zarypov <krre31@gmail.com>
2023-07-30 18:04:09 +0300

    Add hack to fix styling with Qt 6.5 on GTK3

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #include <QApplication>
 
 int main(int argc, char* argv[]) {
+    // Hack to fix styling with Qt 6.5 on GTK3
+    qunsetenv("GNOME_DESKTOP_SESSION_ID");
+    qunsetenv("XDG_CURRENT_DESKTOP");
+
     QApplication app(argc, argv);
 
     app.setOrganizationName(Const::App::Organization);
