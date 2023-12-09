ee2e8ce8ea8ebda443d11e8255bac7b7f9b0583a
Vladimir Zarypov <krre31@gmail.com>
2023-12-09 12:58:05 +0200

    Port project to CMake build system

--- app/main.cpp
+++ app/main.cpp
@@ -7,8 +7,8 @@
 
 int main(int argc, char* argv[]) {
     // Hack to fix styling with Qt 6.5 on GTK3
-    qunsetenv("GNOME_DESKTOP_SESSION_ID");
-    qunsetenv("XDG_CURRENT_DESKTOP");
+    // qunsetenv("GNOME_DESKTOP_SESSION_ID");
+    // qunsetenv("XDG_CURRENT_DESKTOP");
 
     QApplication app(argc, argv);
 
