e3cad1f227f9a87c93853e223e026de90da28088
Vladimir Zarypov <krre31@gmail.com>
2024-08-17 05:40:02 +0300

    Remove all files

--- src/core/Application.cpp
+++
@@ -1,7 +0,0 @@
-#include "Application.h"
-
-Application::Application(int& argc, char* argv[]) : QApplication(argc, argv) {
-    setOrganizationName(Organization);
-    setApplicationName(Name);
-    setApplicationVersion(Version);
-}
