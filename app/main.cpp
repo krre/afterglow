8e6286a413cffb3a6665b328c38345f2941acf5f
Vladimir Zarypov <krre31@gmail.com>
2024-09-28 09:25:37 +0300

    Remove GTK styling hack

--- app/main.cpp
+++ app/main.cpp
@@ -5,12 +5,6 @@
 #include "core/Settings.h"
 
 int main(int argc, char* argv[]) {
-#ifdef Q_OS_LINUX
-    // Hack to fix styling with Qt 6.5 on GTK3
-    qunsetenv("GNOME_DESKTOP_SESSION_ID");
-    qunsetenv("XDG_CURRENT_DESKTOP");
-#endif
-
     Application app(argc, argv);
 
     Global::init();
