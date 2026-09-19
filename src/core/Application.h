1b2243643dad20fcd10b1b913dfaecead7366b38
Vladimir Zarypov <krre31@gmail.com>
2026-09-19 16:21:00 +0300

    Remove duplication of URL constants

--- src/core/Application.h
+++ src/core/Application.h
@@ -8,8 +8,8 @@ public:
     static constexpr auto Name = "Afterglow";
     static constexpr auto Version = PROJECT_VERSION;
     static constexpr auto Status = "Preview Alpha";
-    static constexpr auto Url = "https://github.com/krre/afterglow";
-    static constexpr auto ReleasesUrl = "https://github.com/krre/afterglow/releases";
+    inline static const auto Url = QStringLiteral("https://github.com/krre/afterglow");
+    inline static const auto ReleasesUrl = Url + QStringLiteral("/releases");
     static constexpr auto PrefsName = "prefs.json";
     static constexpr auto Years = "2018-2026";
     static constexpr auto BuildDate = __DATE__;
